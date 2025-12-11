c5685876d8fe8fe9cbe85e3c16bf012da5e71e0c
sj3819 <sj3819@nyu.edu>
2025-12-11 15:09:20 +0800

    update

--- project-b/sketch.js
+++ project-b/sketch.js
@@ -1,49 +1,93 @@
 let particles = [];
 let memories = [];
 let photos = [];
-let revealSize = 260;
+let sound = [];
+
+// Camera controls
+let camRotX = 0;
+let camRotY = 0;
+let zoom = 600;
+let lastMouseX = 0;
+let lastMouseY = 0;
 
 function preload() {
-  photos.push(loadImage("assets/DSCF9399.JPG"));
-  photos.push(loadImage("assets/DSCF3267.JPG"));
-  photos.push(loadImage("assets/DSCF1683.JPG"));
-  photos.push(loadImage("assets/concert.jpg"));
-  photos.push(loadImage("assets/01000407.JPG"));
-  photos.push(loadImage("assets/ballet.JPG"));
-  photos.push(loadImage("assets/beach.jpg"));
-  photos.push(loadImage("assets/DSCF0204.JPG"));
-  photos.push(loadImage("assets/DSCF3175.JPG"));
-  photos.push(loadImage("assets/DSCF4484.JPG"));
-  photos.push(loadImage("assets/DSCF4518.JPG"));
-  photos.push(loadImage("assets/DSCF5207.JPG"));
-  photos.push(loadImage("assets/DSCF5228.JPG"));
-  photos.push(loadImage("assets/DSCF7163.JPG"));
-  photos.push(loadImage("assets/DSCF7195.JPG"));
-  photos.push(loadImage("assets/DSCF7408.JPG"));
-  photos.push(loadImage("assets/DSCF9493.JPG"));
-  photos.push(loadImage("assets/IMG_0888.JPG"));
-  photos.push(loadImage("assets/IMG_1325.JPG"));
-  photos.push(loadImage("assets/IMG_3250.JPG"));
-  photos.push(loadImage("assets/IMG_4030.JPG"));
-  photos.push(loadImage("assets/IMG_4669.JPG"));
-  photos.push(loadImage("assets/IMG_6244.jpeg"));
-  photos.push(loadImage("assets/lantern.jpg"));
-  photos.push(loadImage("assets/photobooth.jpg"));
-  photos.push(loadImage("assets/photoshoot.jpg"));
+  photos.push(loadImage("assets/p1.jpg"));
+  photos.push(loadImage("assets/p2.jpg"));
+  photos.push(loadImage("assets/p3.jpg"));
+  photos.push(loadImage("assets/p4.jpg"));
+  photos.push(loadImage("assets/p5.jpg"));
+  photos.push(loadImage("assets/p6.jpg"));
+  photos.push(loadImage("assets/p7.jpg"));
+  photos.push(loadImage("assets/p8.jpg"));
+  photos.push(loadImage("assets/p9.jpg"));
+  photos.push(loadImage("assets/p10.jpg"));
+  photos.push(loadImage("assets/p11.jpg"));
+  photos.push(loadImage("assets/p12.jpg"));
+  photos.push(loadImage("assets/p13.jpg"));
+  photos.push(loadImage("assets/p14.jpg"));
+  photos.push(loadImage("assets/p15.jpg"));
+  photos.push(loadImage("assets/p16.jpg"));
+  photos.push(loadImage("assets/p17.jpg"));
+  photos.push(loadImage("assets/p18.jpg"));
+  photos.push(loadImage("assets/p19.jpg"));
+  photos.push(loadImage("assets/p20.jpg"));
+  photos.push(loadImage("assets/p21.jpg"));
+  photos.push(loadImage("assets/p22.jpg"));
+  photos.push(loadImage("assets/p23.jpg"));
+  photos.push(loadImage("assets/p24.jpg"));
+  photos.push(loadImage("assets/p25.jpg"));
+  photos.push(loadImage("assets/p26.jpg"));
+  photos.push(loadImage("assets/p27.jpg"));
+  photos.push(loadImage("assets/p28.jpg"));
+  photos.push(loadImage("assets/p29.jpg"));
+  photos.push(loadImage("assets/p30.jpg"));
+  // photos.push(loadImage("assets/p31.jpg"));
+  // photos.push(loadImage("assets/p32.jpg"));
+  // photos.push(loadImage("assets/p33.jpg"));
+  // photos.push(loadImage("assets/p34.jpg"));
+  // photos.push(loadImage("assets/p35.jpg"));
+  // photos.push(loadImage("assets/p36.jpg"));
+  // photos.push(loadImage("assets/p37.jpg"));
+  // photos.push(loadImage("assets/p38.jpg"));
+  // photos.push(loadImage("assets/p39.jpg"));
+  // photos.push(loadImage("assets/p40.jpg"));
+  // photos.push(loadImage("assets/p41.jpg"));
+  // photos.push(loadImage("assets/p42.jpg"));
+  // photos.push(loadImage("assets/p43.jpg"));
+  // photos.push(loadImage("assets/p44.jpg"));
+  // photos.push(loadImage("assets/p45.jpg"));
+  // photos.push(loadImage("assets/p46.jpg"));
+
+  sound.push(loadSound("assets/click.mp3"));
+  sound.push(loadSound("assets/Photograph.mp3"));
 }
+// display text
+// fill(255);
+// text("Zoom into each particle to see each unique memory.", 10, 20);
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
 
-  for (let i = 0; i < 120; i++) {
+  // Play background song
+  if (sound[2].isLoaded()) {
+    sound[2].loop();
+    sound[2].setVolume(0.3);
+  }
+
+  for (let i = 0; i < 31; i++) {
+    let img = random(photos);
+
     particles.push({
-      x: random(width),
-      y: random(height),
-      r: random(3, 5),
-      vx: random(-0.5, 0.5),
-      vy: random(-0.5, 0.5),
-      img: random(photos)
+      x: random(-width / 2, width / 2),
+      y: random(-height / 2, height / 2),
+      z: random(-200, 200),
+      r: random(3, 6),
+      vx: random(-0.6, 0.6),
+      vy: random(-0.6, 0.6),
+      vz: random(-0.3, 0.3),
+      img: img,
+      activeMemory: null
     });
   }
 }
@@ -51,24 +95,68 @@ function setup() {
 function draw() {
   background(10, 10, 15);
 
+  // Camera rotation
+  translate(0, 0, -zoom);
+  rotateX(camRotX);
+  rotateY(camRotY);
+
   drawConnections();
   drawParticles();
 
-  for (let m of memories) {
+  // Update & display memories
+  for (let i = memories.length - 1; i >= 0; i--) {
+    let m = memories[i];
+    m.update();
     m.display();
+
+    if (m.lifespan <= 0) {
+      m.particle.activeMemory = null;
+      memories.splice(i, 1);
+    }
+  }
+}
+
+// CLICK TO REVEAL — One memory per particle
+function mousePressed() {
+  lastMouseX = mouseX;
+  lastMouseY = mouseY;
+
+  for (let p of particles) {
+    let screenPos = createVector();
+    screenPos = screenPosition(p.x, p.y, p.z);
+
+    let d = dist(mouseX - width / 2, mouseY - height / 2, screenPos.x, screenPos.y);
+
+    if (d < p.r + 8 && p.activeMemory == null) {
+      let mem = new PixelMemory(p, p.img);
+      p.activeMemory = mem;
+      memories.push(mem);
+
+      if (sound[0].isLoaded()) sound[0].play();
+      if (sound[1].isLoaded()) setTimeout(() => sound[1].play(), 200);
+
+      break;
+    }
   }
 }
 
 function drawParticles() {
+  noStroke();
+  fill(180, 200, 255, 150);
+
   for (let p of particles) {
     p.x += p.vx;
     p.y += p.vy;
+    p.z += p.vz;
 
-    if (p.x < 0 || p.x > width) p.vx *= -1;
-    if (p.y < 0 || p.y > height) p.vy *= -1;
+    if (p.x < -width / 2 || p.x > width / 2) p.vx *= -1;
+    if (p.y < -height / 2 || p.y > height / 2) p.vy *= -1;
+    if (p.z < -200 || p.z > 200) p.vz *= -1;
 
-    fill(180, 200, 255, 150);
-    ellipse(p.x, p.y, p.r * 2);
+    push();
+    translate(p.x, p.y, p.z);
+    sphere(p.r);
+    pop();
   }
 }
 
@@ -80,75 +168,100 @@ function drawConnections() {
     for (let j = i + 1; j < particles.length; j++) {
       let a = particles[i];
       let b = particles[j];
-      let d = dist(a.x, a.y, b.x, b.y);
+      let d = dist(a.x, a.y, a.z, b.x, b.y, b.z);
 
-      if (d < 110) {
-        line(a.x, a.y, b.x, b.y);
-      }
+      if (d < 120) line(a.x, a.y, a.z, b.x, b.y, b.z);
     }
   }
 }
 
-function mousePressed() {
-  for (let p of particles) {
-    let d = dist(mouseX, mouseY, p.x, p.y);
-
-    if (d < p.r + 8) {
-      memories.push(new PixelMemory(p.x, p.y, p.img));
-      break;
-    }
-  }
-}
-
+// MEMORY CLASS
 class PixelMemory {
-  constructor(x, y, img) {
+  constructor(particle, img) {
+    this.particle = particle;
     this.img = img;
-    this.pixels = [];
-    this.size = 2;
+    this.lifespan = 600;
+    this.fadeStart = 200;
+    this.pixelSize = 4;
+    this.pixelsPerFrame = 230;
+    this.revealSize = random(280, 420);
 
-    // scale factor to fit screen if needed
-    let maxPopupWidth = min(width * 0.5, revealSize);
-    let maxPopupHeight = min(height * 0.5, revealSize);
-    this.scaleFactor = min(maxPopupWidth / img.width, maxPopupHeight / img.height);
+    let maxW = this.revealSize;
+    let maxH = this.revealSize;
+    this.scaleFactor = min(maxW / img.width, maxH / img.height);
 
-    this.displayWidth = img.width * this.scaleFactor;
-    this.displayHeight = img.height * this.scaleFactor;
-
-    // clamp popup to screen bounds
-    this.x = constrain(x - this.displayWidth / 2, 0, width - this.displayWidth);
-    this.y = constrain(y - this.displayHeight / 2, 0, height - this.displayHeight);
+    this.displayW = img.width * this.scaleFactor;
+    this.displayH = img.height * this.scaleFactor;
 
     img.loadPixels();
+    this.pixels = [];
+    for (let yy = 0; yy < this.displayH; yy += this.pixelSize) {
+      for (let xx = 0; xx < this.displayW; xx += this.pixelSize) {
+        let ix = floor(xx / this.scaleFactor);
+        let iy = floor(yy / this.scaleFactor);
+        let idx = (ix + iy * img.width) * 4;
 
-    for (let yy = 0; yy < this.displayHeight; yy += this.size) {
-      for (let xx = 0; xx < this.displayWidth; xx += this.size) {
-        let origX = floor(xx / this.scaleFactor);
-        let origY = floor(yy / this.scaleFactor);
-        let idx = (origX + origY * img.width) * 4;
-
-        let r = img.pixels[idx];
-        let g = img.pixels[idx + 1];
-        let b = img.pixels[idx + 2];
-
-        this.pixels.push({ x: xx, y: yy, r, g, b });
+        this.pixels.push({
+          x: xx,
+          y: yy,
+          r: img.pixels[idx],
+          g: img.pixels[idx + 1],
+          b: img.pixels[idx + 2]
+        });
       }
     }
+    this.pixels = shuffle(this.pixels);
+    this.currentIndex = 0;
+  }
+
+  update() {
+    this.lifespan--;
   }
 
   display() {
     push();
-    translate(this.x, this.y);
 
-    for (let p of this.pixels) {
-      fill(p.r, p.g, p.b);
-      noStroke();
-      rect(p.x, p.y, this.size, this.size);
+    let alpha = 255;
+    if (this.lifespan < this.fadeStart)
+      alpha = map(this.lifespan, 0, this.fadeStart, 0, 255);
+
+    let shrink = 1;
+    if (this.lifespan < this.fadeStart)
+      shrink = map(this.lifespan, 0, this.fadeStart, 0, 1);
+
+    translate(this.particle.x, this.particle.y, this.particle.z);
+    scale(shrink);
+    translate(-this.displayW / 2, -this.displayH / 2);
+
+    let endIndex = min(this.currentIndex + this.pixelsPerFrame, this.pixels.length);
+
+    noStroke();
+    for (let i = 0; i < endIndex; i++) {
+      let p = this.pixels[i];
+      fill(p.r, p.g, p.b, alpha);
+      rect(p.x, p.y, this.pixelSize, this.pixelSize);
     }
+
+    this.currentIndex = endIndex;
 
     pop();
   }
 }
 
+// Camera drag to rotate
+function mouseDragged() {
+  let sensitivity = 0.005;
+  camRotY += (mouseX - lastMouseX) * sensitivity;
+  camRotX += (mouseY - lastMouseY) * sensitivity;
+  lastMouseX = mouseX;
+  lastMouseY = mouseY;
+}
+
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+function keyPressed() {
+  if (key === '+' || key === '=') zoom -= 100;
+  if (key === '-' || key === '_') zoom += 100;
 }
